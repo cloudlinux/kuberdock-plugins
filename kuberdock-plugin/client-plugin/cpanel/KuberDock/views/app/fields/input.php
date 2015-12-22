42e5810d9793dfcc3050465e040596199e59ed6e
Oleg Bednarskiy <obednarsky@cloudlinux.com>
2015-12-22 13:39:49 +0200

    AC-1803: add style to preapp install page (cpanel client plugin)

--- kuberdock-plugin/client-plugin/cpanel/KuberDock/views/app/fields/input.php
+++ kuberdock-plugin/client-plugin/cpanel/KuberDock/views/app/fields/input.php
@@ -1,7 +1,7 @@
 <div class="form-group">
-    <label for="<?php echo $variable?>" class="col-sm-3 control-label"><?php echo $data['description']?></label>
+    <label for="<?php echo $variable?>" class="col-sm-12"><?php echo $data['description']?></label>
 
-    <div class="col-sm-4">
+    <div class="col-sm-5">
         <input type="text" name="<?php echo $variable?>" id="<?php echo $variable?>" value="<?php echo $data['default']?>">
     </div>
 </div>
--- kuberdock-plugin/client-plugin/cpanel/KuberDock/views/app/installPredefined.php
+++ kuberdock-plugin/client-plugin/cpanel/KuberDock/views/app/installPredefined.php
@@ -2,14 +2,14 @@
 <script src="assets/script/slider/jquery.nouislider.all.min.js"></script>
 <script src="assets/script/install.js"></script>
 
-<div class="container-fluid content">
+<div class="container-fluid content preapp-install-page">
     <div class="row">
-        <p>
-            <?php echo $app->getPreDescription();?>
-        </p>
-
         <div class="page-header">
+            <div class="col-xs-1 nopadding"><span class="header-ico-preapp-install"></span></div>
             <h2><?php echo $app->getName()?></h2>
+        </div>
+        <div class="col-xs-11 col-xs-offset-1 nopadding">
+            <p class="pre-app-desc"><?php echo $app->getPreDescription();?></p>
         </div>
     </div>
 
@@ -20,32 +20,32 @@
         <input type="hidden" name="kube_type" id="kube_type" value="<?php echo $app->getKubeTypeId()?>" data-pid="<?php echo $app->getPackageId(true)?>">
         <input type="hidden" name="total_kube_count" id="total_kube_count" value="<?php echo $app->getTotalKubes()?>">
 
-    <div class="row">
-        <?php foreach(array_keys($variables) as $variable):?>
-            <?php $app->renderVariable($variable, $this->controller);?>
-        <?php endforeach;?>
-    </div>
-
-    <div class="row">
-        <div class="col-sm-5">
-            <div class="product-description"></div>
-        </div>
-    </div>
-
-    <div class="row">
-        <div class="col-md-4">
-            <strong>Total: </strong><span id="priceBlock"></span>
-        </div>
-    </div>
-
-    <div class="row">
         <div class="row">
-            <div class="col-sm-12 text-center">
+            <div class="col-xs-11 col-xs-offset-1 nopadding">
+                <?php foreach(array_keys($variables) as $variable):?>
+                    <?php $app->renderVariable($variable, $this->controller);?>
+                <?php endforeach;?>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-xs-11 col-xs-offset-1 nopadding">
+                <div class="col-sm-5 no-paddind">
+                    <div class="product-description"></div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row total-border-wrapper">
+            <div class="total-border">Total: <span id="priceBlock"></span></div>
+        </div>
+
+        <div class="row" style="margin-bottom: 23px;">
+            <div class="text-right">
                 <button type="submit" class="btn btn-primary">Start your App</button>
                 <div class="ajax-loader buttons hidden"></div>
             </div>
         </div>
-    </div>
     </form>
 </div>
 
