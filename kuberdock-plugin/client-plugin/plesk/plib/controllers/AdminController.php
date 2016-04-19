3cfb02248d15010ada0c37c18ebfdab5af85a4eb
psednev <psednev@cloudlinux.com>
2016-04-19 19:36:05 +0300

    AC-2932 - Plesk. Admin tab - Update kubecli.conf settings
    Change-Id: Ib7794fe123d31b86874a90a8eb2af9c9694edff5

--- kuberdock-plugin/client-plugin/plesk/plib/controllers/AdminController.php
+++ kuberdock-plugin/client-plugin/plesk/plib/controllers/AdminController.php
@@ -2,10 +2,13 @@
 
 class AdminController extends pm_Controller_Action
 {
-    
+    protected $_accessLevel = 'admin';
+
     public function init()
     {
         parent::init();
+
+        require_once pm_Context::getPlibDir() . 'library/KuberDock/init.php';
 
         $this->view->pageTitle = 'KuberDock Extension';
         $this->view->tabs = array(
@@ -34,5 +37,15 @@ class AdminController extends pm_Controller_Action
 
     public function settingsAction()
     {
+        $form = new \Kuberdock\classes\plesk\forms\KubeCli();
+        $model = new \Kuberdock\classes\plesk\models\KubeCli();
+
+        if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
+            $model->save($form->getValues());
+        }
+
+        $form->populate($model->read());
+
+        $this->view->form = $form;
     }
 }
---
+++ kuberdock-plugin/client-plugin/plesk/plib/library/KuberDock/views/plesk/template_root.php
@@ -0,0 +1,16 @@
+[global]
+
+# kuberdock server URL
+url = <?php echo $url;?>
+
+[defaults]
+
+# default registry to pull docker images from
+registry = <?php echo $registry;?>
+
+# default username for kuberdock to log in
+user = <?php echo $user;?>
+
+# default password for kuberdock to log in
+password = <?php echo $password;?>
+
