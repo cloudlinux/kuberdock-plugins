332f861bfb7dd6fc9f585a9241844fa1b378654c
psednev <psednev@cloudlinux.com>
2016-04-21 16:53:22 +0300

    AC-2933 - Plesk. Admin tab - Set default package\kube
    Change-Id: I23d68093df062254bb5bdb86a8081fd078987223

--- kuberdock-plugin/client-plugin/plesk/plib/controllers/AdminController.php
+++ kuberdock-plugin/client-plugin/plesk/plib/controllers/AdminController.php
@@ -33,12 +33,30 @@ class AdminController extends pm_Controller_Action
 
     public function defaultsAction()
     {
+        $this->view->assets = \Kuberdock\classes\Base::model()->getStaticPanel()->getAssets();
+        $this->view->assets->registerScripts(array(
+            'script/lib/jquery.min',
+            'script/admin/defaults',
+        ));
+        $this->view->assets->registerStyles(array('css/admin'));
+
+        $model = new \Kuberdock\classes\plesk\models\Defaults;
+
+        if ($this->getRequest()->isPost()) {
+            $model->save($this->getRequest()->getPost());
+        }
+
+        $data = $model->read();
+
+        $this->view->form = new \Kuberdock\classes\plesk\forms\Defaults();
+        $this->view->packagesKubes = $data['packagesKubes'];
+        $this->view->defaults = $data['defaults'];
     }
 
     public function settingsAction()
     {
-        $form = new \Kuberdock\classes\plesk\forms\KubeCli();
-        $model = new \Kuberdock\classes\plesk\models\KubeCli();
+        $form = new \Kuberdock\classes\plesk\forms\KubeCli;
+        $model = new \Kuberdock\classes\plesk\models\KubeCli;
 
         if ($this->getRequest()->isPost() && $form->isValid($this->getRequest()->getPost())) {
             $model->save($form->getValues());
