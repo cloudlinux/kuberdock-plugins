b41574bfff6b62c9b1c6c5d0e9284604ac58ed3c
Ruslan Rakhmanberdiev <rrakhmanberdiev@cloudlinux.com>
2016-10-03 08:50:37 +0300

    WHMCS. Fix for 5.4
    Change-Id: I9af7716cb51de14516a632dcb6402aafcadc42c5

--- AppCloud-plugin/includes/api/orderkuberdockpod.php
+++ AppCloud-plugin/includes/api/orderkuberdockpod.php
@@ -11,7 +11,8 @@ try {
     $postFields = \base\CL_Tools::getApiParams($vars);
 
     // Used for local api call
-    if (empty((array) $postFields->params) && isset($_POST['client_id']) && isset($_POST['pod'])) {
+    $params = (array) $postFields->params;
+    if (empty($params) && isset($_POST['client_id']) && isset($_POST['pod'])) {
         $postFields->params->client_id = (int) $_POST['client_id'];
         $postFields->params->pod = $_POST['pod'];
         $postFields->params->referer = isset($_POST['referer']) ? $_POST['referer'] : '';
